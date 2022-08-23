b023eb2229f024a0ceed70449d4c0c2794825bb1
hasan <mdhasanhk.98@gmail.com>
2022-08-24 02:39:26 +0600

    add

--- src/App.js
+++ src/App.js
@@ -2,9 +2,23 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const nayoks = ['razzak', 'sakib', 'sium', 'arifin', 'salman']
+  const Products =[
+    { name: 'photoshop', price: '90$'},
+    { name: 'illustrator', price: '60$'}
+    ]
+
   return (
     <div className="App">
       <header className="App-header">
+        <ul>
+          {nayoks.map(nayok => <li>{nayok}</li> )}
+          {Products.map(product => <li>{product.name}</li> )}
+        </ul>
+        {Products.map(product => <Product product={product}></Product>)}
+        {/* <Product product= {Products[0]}></Product>
+        <Product product= {Products[1]}></Product> */}
+        
        <Person name="shakib khan" nayika="bubly"></Person>
        <Person  name="riyaz" nayika="purnima"></Person>
        <Person  name="bapparaz" nayika="mousumi"></Person>
@@ -14,14 +28,34 @@ function App() {
   );
 }
 
+function Product(props) {
+  const productStyle ={
+    border: "1px solid gray",
+    margin: "10px",
+    padding: "10px",
+    height: "300px",
+    with: "300px",
+    backgroundColor: "lightgray"
+
+  }
+  console.log(props)
+  return (
+  <div style= {productStyle}>
+    <h1>{props.product.name}</h1>
+    <h2>{props.product.price}</h2>
+    <button>Buy Now</button>
+  </div>
+  )
+}
+
 function Person(props) {
-  const personStyle = {
+  const personStyles = {
     border: "2px solid red",
     margin: "10px",
     padding: "10px"
   }
   return (
-    <div style={personStyle}>
+    <div style={personStyles}>
       <h1>Name: {props.name}</h1>
       <p>Hero of {props.nayika}</p>
     </div>
